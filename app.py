370bd9f0cb24397e59de7434e9280db7269affa6
lyx <lyxlyxi@hotmail.com>
2022-03-15 22:36:21 +0800

    correct tz

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ scheduler = BlockingScheduler(timezone='Asia/Singapore')
 # GMT-5 > 09:30am - 4pm
 # GMT+8 > 10:30pm - 5am
 trigger = OrTrigger([
-    CronTrigger(second='*/10', day_of_week='mon-fri', minute='30-59', hour='21'),
-    CronTrigger(second='*/10', day_of_week='mon-fri', hour='22-23'),
-    CronTrigger(second='*/10', day_of_week='tue-sat', hour='0-3')
+    CronTrigger(second='*/10', day_of_week='mon-fri', minute='30-59', hour='21', timezone='Asia/Singapore'),
+    CronTrigger(second='*/10', day_of_week='mon-fri', hour='22-23', timezone='Asia/Singapore'),
+    CronTrigger(second='*/10', day_of_week='tue-sat', hour='0-3', timezone='Asia/Singapore')
 ])
 
 scheduler.add_job(getMarketPrice, trigger=trigger, args=[TICKERS], name='getMarketPrice')
