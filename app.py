2b48d03691a375a522c5e5931fe19e6cb6bfde7e
lyx <lyxlyxi@hotmail.com>
2022-03-15 22:10:59 +0800

    start at 930

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ scheduler = BlockingScheduler(timezone='Asia/Singapore')
 # GMT-5 > 09:30am - 4pm
 # GMT+8 > 10:30pm - 5am
 trigger = OrTrigger([
-    CronTrigger(second='*/10', day_of_week='mon-fri', minute='30-59', hour='22-23'),
+    CronTrigger(second='*/10', day_of_week='mon-fri', minute='30-59', hour='21'),
+    CronTrigger(second='*/10', day_of_week='mon-fri', hour='22-23'),
     CronTrigger(second='*/10', day_of_week='tue-sat', hour='0-4')
 ])
 
